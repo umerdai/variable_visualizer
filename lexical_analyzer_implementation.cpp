3220062c68ee6bd8c920c9996fab3eb809e9ba17
bellirumon <anonymsjit7861@gmail.com>
2023-05-28 01:51:38 +0500

    keyword identification fixes

--- lexical_analyzer_implementation.cpp
+++ lexical_analyzer_implementation.cpp
@@ -6,25 +6,26 @@ using namespace std;
 class lexer {
 private:
 
-    string keyword[4] = {"int","char","flaot","string"};
+    string keyword[4] = { "int","char","float","string" };
     char operators[4] = { '+','-','/','*' };
     char punctation[1] = { ';' };
     string identifier[1] = { "cout" };
     char local_input[100];
     unsigned short keyword_counter = 0;
     int j = 0;
-    string temp ;
-    int local_size=0;
-    bool check =false;
+    string temp;
+    int local_size = 0;
+    bool check = false;
 public:
-    lexer(const char* input,int size)
+    lexer(const char* input, int size)
     {
         for (int i = 0; i < size; i++)
         {
+
             local_input[i] = input[i];
             local_size = size;
 
-           
+
         }for (int j = 0; j < local_size; j++)
         {
             cout << local_input[j];
@@ -34,63 +35,69 @@ public:
     };
 
     bool isKeyword(string str) {
-        for (int i = 0; i < local_size; i++)
-        {
-            cout<<str.length() - 1;
-            
-            if (str == keyword[i])
-            {
+        bool flag = false;
 
-                return true;
-            }
-            else
+
+        for (int i = 0; i <= keyword->size(); i++)
+        {
+            /*cout << str.length() - 1;*/
+
+            if (str.compare(keyword[i]) == 0)
             {
-                return false;
+                flag = true;
+                break;
             }
-       }
+        }
+        return flag;
     }
 
     void checkingkeyword()
     {
-       
+
         for (int i = 0; i < local_size; i++)
         {
-            temp += local_input[i];
-            if (local_input[i] == ' ')
-            {
 
-                temp[i] = '\0';
+            if (local_input[i] != ' ')
+            {
+                temp += local_input[i];
+
+            }
+            else {
+
                 bool check = isKeyword(temp);
                 if (check)
                 {
-                    cout << temp.length() <<" is a keyword: " << endl;
+                    cout << temp << " is a keyword: " << endl;
+                    temp = "";
                 }
                 else
                 {
-                    cout << temp.length() << " is not a keyword: " << endl;
+                    cout << temp << " is not a keyword: " << endl;
+                    temp = "";
                 }
-               
             }
-        
-        }
-       /* 
-        local_input.erase(std::remove(local_input.begin(), local_input.end(), ' '), local_input.end());
 
-            if (isdigit(c))  {
-                cout << "is a number";
-                keyword_counter++;
-            }
-            else {
-                cout << c << " is not a number." << std::endl;
-            }*/
-          
+        }
+        /*
+         local_input.erase(std::remove(local_input.begin(), local_input.end(), ' '), local_input.end());
+
+             if (isdigit(c))  {
+                 cout << "is a number";
+                 keyword_counter++;
+             }
+             else {
+                 cout << c << " is not a number." << std::endl;
+             }*/
+
     }
 };
 
-void main()
+int main()
 {
-   
-    char str[100] = "int a=3; int b=4; cout<<a+b; char char";
-    lexer l(str,33);
 
+    char str[100] = "int a=3; int b=4; cout<<a+b; char char ";
+
+    lexer l(str, strlen(str));
+
+    return 0;
 }
